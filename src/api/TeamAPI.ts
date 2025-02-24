2f41bb4f726c6cf1eb7892b9150bb695a106bf38
lucascostamagna19@outlook.com <lucascostamagna19@outlook.com>
2025-02-24 20:02:05 -0300

    fix bug

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -4,7 +4,7 @@ import { Project, TeamMember, TeamMemberForm, teamMembersSchema } from "../types
 
 export async function findUserByEmail({projectId, formData} : {projectId: Project['_id'], formData: TeamMemberForm}) {
     try {
-        const { data } = await api.post<string>(`/projects/${projectId}/team/find`, formData);
+        const { data } = await api.post(`/projects/${projectId}/team/find`, formData);
         return data;
     }
     catch (error) {
